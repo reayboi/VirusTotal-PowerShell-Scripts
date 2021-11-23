f3273dcb19d5aae638c68f28780475dda10ae544
Brandon Reay <40546925+reayboi@users.noreply.github.com>
2021-11-23 17:38:16 +0000

    1.1 - Changes made to url scan

--- vt_url_scan.ps1
+++ vt_url_scan.ps1
@@ -12,28 +12,45 @@ $URL = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
 #$request_url = "https://www.virustotal.com/api/v3/urls/" + $hash
 
 $Bytes = [Text.Encoding]::UTF8.GetBytes($URL)
+
 $s = [Convert]::ToBase64String($Bytes)
 $s = $s.Split('=')[0]
 $s = $s.Replace('+', '-').Replace('/','_')
 
 $request_url = "https://www.virustotal.com/api/v3/urls/" + $s
 
-Write-Output $request_url
-
-$api_key = "ea6ed29f201e4b06847e464f8c7e2f173c563dcd2a9e62effe80918ccbfd74b8"
+#Write-Output $request_url
 
 $headers = @{}
-
 $headers.Add("Accept", "application/json")
-
 $headers.Add("x-apikey", "ea6ed29f201e4b06847e464f8c7e2f173c563dcd2a9e62effe80918ccbfd74b8")
-
-#$response = Invoke-WebRequest -Uri 'https://www.virustotal.com/api/v3/urls/8d5f80f3d7f7aa77c1ae9ab34a0c94b920eb4e2168d8866c6a4528cea58021c0' -Method GET -Headers $headers
 
 $response = Invoke-WebRequest -Uri $request_url -Method GET -Headers $headers
 
 $request_content = $response.Content
-
 $request_content = $request_content | ConvertFrom-Json
 
-Write-Output $request_content.data.attributes.title
+$benign = [int]$request_content.data.attributes.last_analysis_stats.harmless
+$malicious = [int]$request_content.data.attributes.last_analysis_stats.malicious
+
+$last_submission_date = [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($request_content.data.attributes.last_submission_date))
+
+
+Write-Output "VirusTotal Investigation for $($URL)"
+
+if ($malicious -le 0)
+{
+    Write-Output "No security vendors flagged this URL as malicious"
+}
+else
+{
+    Write-Output "$($malicious) security vendors flagged this domain as malicious"
+}
+
+#Write-Output "Last Analysis Statistics:`nBenign classifications - $($benign), Malicious classifications - $($malicious)"
+
+Write-Output "$($malicious) / $($malicious+$benign)"
+
+Write-Output "Last submission date: $($last_submission_date)"
+
+
